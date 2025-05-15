56b32f2245c663fb5939671c71c11355e0f1d52e
qiqi <1610494022@qq.com>
2025-05-15 22:59:19 +0800

    第六次提交

--- 前端/node_modules/element-plus/es/components/icon/index.mjs
+++
@@ -1,8 +0,0 @@
-import Icon from './src/icon.mjs';
-export { iconProps } from './src/icon2.mjs';
-import { withInstall } from '../../utils/vue/install.mjs';
-
-const ElIcon = withInstall(Icon);
-
-export { ElIcon, ElIcon as default };
-//# sourceMappingURL=index.mjs.map
